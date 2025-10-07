75b493297de95fc7efe6bc27d02d843f778f3506
Emmanuel Daniel <emmanueldanielmaziku@gmail.com>
2025-10-08 02:36:42 +0300

    UAT Week6

--- app/api/products/count/route.ts
+++ app/api/products/count/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import { cookies } from "next/headers";
-import { handleAuthenticationError } from "../utils/authErrorHandler";
+import { handleAuthenticationError } from "../../utils/authErrorHandler";
 
 const API_BASE_URL = "https://tccia.kalen.co.tz";
 
