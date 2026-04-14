0cbeeffc7323ee5a10152ea208e4cc3f6bbc2297
emmanueldanielmaziku <emmanueldanielmaziku@gmail.com>
2026-04-14 11:02:39 +0300

    Dev URL

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 
-const API_BASE_URL = "https://staff.tncc.co.tz";
+const API_BASE_URL = "https://tccia.kalen.co.tz";
 
 export async function POST(request: Request) {
   try {
